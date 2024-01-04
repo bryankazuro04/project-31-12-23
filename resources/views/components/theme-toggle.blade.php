47878301bfee7e933718b8f666419fe3e54e2963
bryankazuro04 <mhdalana211201@gmail.com>
2024-01-05 00:57:18 +0700

    add attribute class

--- resources/views/components/theme-toggle.blade.php
+++ resources/views/components/theme-toggle.blade.php
@@ -1,4 +1,4 @@
-<section>
+<section {{ $attributes->merge(['class' => '']) }}>
     <input type="checkbox" name="theme-toggle" id="theme-toggle" class="theme-toggle sr-only">
     <label for="theme-toggle"
         class="flex justify-center items-center w-8 h-8 p-2 bg-slate-100 hover:bg-slate-200 dark:bg-slate-700 dark:hover:bg-slate-600/80 rounded-full cursor-pointer hover:rotate-180 duration-300 transition">
