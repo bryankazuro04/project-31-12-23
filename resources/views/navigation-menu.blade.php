dc89b2186c6181a3056cac3279e6ca1fc69699bb
bryankazuro04 <mhdalana211201@gmail.com>
2024-01-05 00:57:00 +0700

    add theme toggle for mobile size

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,7 +5,7 @@
             <div class="flex justify-between h-16">
                 <div class="flex">
                     <!-- Logo -->
-                    <div class="shrink-0 flex items-center">
+                    <div class="xs:shrink-0 flex items-center">
                         <a href="{{ route('dashboard') }}" class="flex items-center gap-4">
                             {{-- <x-application-mark class="block h-9 w-auto" />
                             <h1 class="text-white font-bold">Brand</h1> --}}
@@ -128,9 +128,12 @@
                         </x-dropdown>
                     </div>
                 </div>
-    
+
+                
                 <!-- Hamburger -->
-                <div class="-me-2 flex items-center sm:hidden">
+                <div class="-me-2 flex items-center gap-3 sm:hidden">
+                    <x-theme-toggle class="sm:hidden" />
+
                     <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                         <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                             <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
