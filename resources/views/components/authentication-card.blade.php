f03838ad95d9d1b1676e8886f390c478756916e0
bryankazuro04 <mhdalana211201@gmail.com>
2024-01-05 12:06:19 +0700

    add img logo

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,8 @@
-<div class="h-screen pt-6 sm:pt-0 relative grid place-items-center">
-    {{-- <div>
-        {{ $logo }}
-    </div> --}}
-
-    <div class="w-full sm:max-w-md px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+<div class="h-screen mx-4 pt-6 sm:pt-0 relative grid place-items-center">
+    <div>
+        <img src="{{ asset('images/logo-pelindo.png') }}" alt="Pelindo Logo">
+    </div>
+    <div class="w-full sm:max-w-md -mt-56 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden rounded-lg">
         {{ $slot }}
     </div>
 </div>
