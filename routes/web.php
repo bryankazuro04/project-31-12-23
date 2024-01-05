f97205a1bff57c0820a7b0312bc9a614988c45bd
bryankazuro04 <mhdalana211201@gmail.com>
2024-01-05 12:05:40 +0700

    make redirect root url

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', function () {
