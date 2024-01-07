4a65e03f2d49d7fb77cd5d13fd76f5aae0982fed
bryankazuro04 <mhdalana211201@gmail.com>
2024-01-07 22:18:08 +0700

    make chart element

---
+++ resources/views/components/dashboard/productivity-chart.blade.php
@@ -0,0 +1,11 @@
+<div class="flex flex-col col-span-full sm:col-span-6 bg-white dark:bg-slate-800 shadow-md rounded-lg border border-slate-200 dark:border-slate-700">
+  <header class="px-5 py-4 border-b border-slate-100 dark:border-slate-700">
+    <h2 class="font-semibold text-slate-800 dark:text-slate-100">Productivity Data</h2>
+  </header>
+
+  <div id="productivity-data-legend" class="px-5 py-3">
+    <ul class="flex flex-wrap"></ul>
+  </div>
+
+  <div class="grow"><canvas id="productivityChart" width="400" height="200"></canvas></div>
+</div>
---
+++ resources/views/components/dashboard/traffic/traffic-jumlah_bongkar_muat-chart.blade.php
@@ -0,0 +1,13 @@
+<div class="flex flex-col col-span-full sm:col-span-6 bg-white dark:bg-slate-800 shadow-md rounded-lg border border-slate-200 dark:border-slate-700">
+  <header class="px-5 py-4 border-b border-slate-100 dark:border-slate-700">
+    <h2 class="font-semibold text-slate-800 dark:text-slate-100">Traffic Data</h2>
+  </header>
+
+  <h3 class="text-center text-slate-400 font-bold mt-2">Jumlah Bongkar Muat</h3>
+  
+  <div id="traffic-jumlah_bongkar_muat-data-legend" class="px-5 py-3">
+    <ul class="flex flex-wrap justify-center"></ul>
+  </div>
+
+  <div class="grow"><canvas id="trafficJumlahBongkarMuatChart" width="400" height="200"></canvas></div>
+</div>
