0431b2604c5b8c1b6d132805d64f39d5e390f9fd
bryankazuro04 <mhdalana211201@gmail.com>
2024-01-07 22:18:18 +0700

    add chart element

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,19 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+      {{ __('Dashboard') }}
+    </h2>
+  </x-slot>
 
-    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-        <x-welcome />
-    </div>
+
+  <div class="grid grid-cols-12 gap-6 mb-4">
+    <x-dashboard.traffic.traffic-kunjungan_kapal-chart />
+    <x-dashboard.traffic.traffic-jumlah_bongkar_muat-chart />
+    <x-dashboard.traffic.traffic-grt-chart />
+    <x-dashboard.traffic.traffic-loa-chart />
+    <x-dashboard.service-chart />
+    <x-dashboard.utilization.utilization-occupancy_ratio-chart />
+    <x-dashboard.utilization.utilization-through_put-chart />
+    <x-dashboard.productivity-chart />
+  </div>
 </x-app-layout>
